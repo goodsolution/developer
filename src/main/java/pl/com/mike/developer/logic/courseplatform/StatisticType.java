e602732fe1122b8243464ab7f42e94221e508dc9
michalswig <mike@data.pl>
2023-04-17 11:13:05 +0200

    initial project structure

--- src/main/java/pl/com/mike/developer/logic/courseplatform/StatisticType.java
+++
@@ -1,9 +0,0 @@
-package pl.com.mike.developer.logic.courseplatform;
-
-public enum StatisticType {
-    NUMBER_OF_VISIBLE_COURSES,
-
-    NUMBER_OF_NO_FREE_ORDERS,
-    NUMBER_OF_FREE_ORDERS,
-    REGISTERED_CUSTOMERS_IN_LAST
-}
