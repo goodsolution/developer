3939b5fe14716d1c7509048d52b90681b451c2a2
michalswig <mike@data.pl>
2023-12-06 16:16:10 +0100

    header module

--- developer-angular/src/app/modules/core/core.module.ts
+++ developer-angular/src/app/modules/core/core.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterModule} from "@angular/router";
 
 
 
@@ -12,7 +16,11 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule
   ]
 })
 export class CoreModule { }
