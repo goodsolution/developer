9adeb2cd34dbb385991a61fba4614f4489044fdd
michalswig <mike@data.pl>
2023-12-06 11:51:58 +0100

    enviroments, bootstrap

--- developer-angular/src/environments/environment.development.ts
+++ developer-angular/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   logoDeveloperEndpoint: 'https://localhost:8081/api/dev/developers/logo',
   frontMenuDeveloperEndpoint: 'https://localhost:8081/api/dev/developers/front-menu',
   developerEndpoint: 'https://localhost:8081/api/dev/developers/code'
