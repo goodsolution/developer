e66df1cc9bec78047fff4b6ab1e20f895ddac685
michalswig <mike@data.pl>
2023-12-07 16:59:50 +0100

    Header Menu

--- developer-angular/src/app/modules/core/services/header-menu.service.ts
+++ developer-angular/src/app/modules/core/services/header-menu.service.ts
@@ -12,21 +12,24 @@ export class HeaderMenuService {
   apiUrl = environment.frontMenuDeveloperEndpoint;
   constructor(private http: HttpClient) { }
 
-  getMenu(): Observable<HeaderMenu[]>{
-    return this.http.get<HeaderMenuResponse[]>(`${this.apiUrl}`).pipe(
-      map((menusResponse) =>
-        menusResponse.map(
-          ({name, url, children}) =>
-            new HeaderMenu(
-              name,
-              url,
-              children
-            )
-        )
-      )
-    )
+  // getMenu(): Observable<HeaderMenu[]>{
+  //   return this.http.get<HeaderMenuResponse[]>(`${this.apiUrl}`).pipe(
+  //     map((menusResponse) =>
+  //       menusResponse.map(
+  //         ({name, url, children}) =>
+  //           new HeaderMenu(
+  //             name,
+  //             url,
+  //             children
+  //           )
+  //       )
+  //     )
+  //   )
+  // };
 
-  };
+  getMenu():Observable<HeaderMenuResponse[]>{
+    return this.http.get<HeaderMenuResponse[]>(`${this.apiUrl}`)
+  }
 
 
 }
