038a57f5eaf9805869d29b3fa76f94d0637a132c
michalswig <mike@data.pl>
2023-12-31 11:20:40 +0100

    add angular material module and vendor.js lib file during build in  a src angular project

---
+++ src-angular/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  logoDeveloperEndpoint: 'https://localhost:8081/api/system/developers/logo',
+  frontMenuDeveloperEndpoint: 'https://localhost:8081/api/system/developers/front-menu',
+  systemCodeEndpoint: 'https://localhost:8081/api/system/developers/code',
+  homePagePhoto: 'https://localhost:8081/api/system/homePageImagePath'
+};
