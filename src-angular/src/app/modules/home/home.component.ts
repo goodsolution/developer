0d78dd0967e3cfeadc5013c4f1d67ee52b2dfc75
michalswig <mike@data.pl>
2024-01-04 13:19:35 +0100

    add home page photo

--- src-angular/src/app/modules/home/home.component.ts
+++ src-angular/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CodeStatusService} from "../core/services/code-status.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  code!: string;
+
+  constructor(
+    private codeService: CodeStatusService) {
+  }
+
+  getCode() {
+    this.codeService.getDeveloperStatus().subscribe(data => {
+      this.code = data.code;
+    });
+  }
+
+  ngOnInit(): void {
+    this.getCode();
+  }
 
 }
