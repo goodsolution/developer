343623e3cb4bc559dd2851c508e0845b2c8afa12
michalswig <mike@data.pl>
2024-01-15 22:35:40 +0100

    investment-list.component cache

--- src-angular/src/app/modules/core/services/cities.service.ts
+++ src-angular/src/app/modules/core/services/cities.service.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../../../environments/environment.development";
 import {HttpClient} from "@angular/common/http";
-import {Observable, of, tap} from "rxjs";
+import {interval, Observable, of, switchMap, tap} from "rxjs";
 import {SearchResultCityModel} from "../models/searchResultCity.model";
-import {CityResponse} from "../models/city.model";
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +10,79 @@ import {CityResponse} from "../models/city.model";
 export class CitiesService {
   private apiUrl = environment.citiesEndpoint;
   private cache!: SearchResultCityModel;
+  private lastUpdated: number = 0;
+  private updateInterval = 60000; // 15 sec in milliseconds
 
   constructor(private http: HttpClient) {
+    this.startPeriodicUpdate()
+  }
+
+  private startPeriodicUpdate(): void {
+    console.log('Starting periodic update Cities 60 sec interval');
+    interval(this.updateInterval).pipe(
+      switchMap(() => {
+        return this.fetchDataFromAPI();
+      })
+    ).subscribe();
   }
 
   getCities(): Observable<SearchResultCityModel> {
-    if (this.cache) {
+    // if (this.cache && (new Date().getTime() - this.lastUpdated < this.updateInterval)) {
+    //   console.log(this.lastUpdated);
+    //   console.log(this.updateInterval);
+    //   console.log(new Date().getTime());
+    //   console.log(new Date().getTime() - this.lastUpdated);
+    //   console.log('Fetching data from cache Cities');
+    //   return of(this.cache); // Return cached data
+    // } else {
+    //   console.log('Fetching data from API Cities');
+    //   return this.fetchDataFromAPI();
+    // }
+    if(this.cache){
       console.log('Fetching data from cache Cities');
-      return of(this.cache); // Return cached data
+      return of(this.cache);
     } else {
       console.log('Fetching data from API Cities');
-      return this.http.get<SearchResultCityModel>(`${this.apiUrl}`)
-        .pipe(tap(data => this.cache = data));
+      return this.fetchDataFromAPI();
     }
   }
+
+  private fetchDataFromAPI(): Observable<SearchResultCityModel> {
+    return this.http.get<SearchResultCityModel>(`${this.apiUrl}`).pipe(
+      tap(data => {
+        if (this.isDataUpdated(data)) {
+          this.cache = data;
+          this.lastUpdated = new Date().getTime();
+        } else {
+          console.log('cities- No changes in data, cache not updated');
+        }
+      })
+    );
+  }
+
+  private isDataUpdated(newData: SearchResultCityModel): boolean {
+    if (!this.cache || this.cache.cities.length !== newData.cities.length) {
+      console.log('CIty is DataUpdated Cache is empty or data length is different')
+      return true;
+    }
+    // Create sets of IDs for easy comparison
+    const cachedIds = new Set(this.cache.cities.map(investment => investment.id));
+    const newIds = new Set(newData.cities.map(investment => investment.id));
+    // Check if every ID in the new data exists in the cache
+    for (const id of newIds) {
+      if (!cachedIds.has(id)) {
+        return true;
+      }
+    }
+    // Check if every ID in the cache exists in the new data
+    for (const id of cachedIds) {
+      if (!newIds.has(id)) {
+        return true;
+      }
+    }
+    // If no changes are found
+    return false;
+  }
+
 
 }
--- src-angular/src/app/modules/core/services/investments.service.ts
+++ src-angular/src/app/modules/core/services/investments.service.ts
@@ -1,30 +1,40 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../../../environments/environment.development";
 import {HttpClient} from "@angular/common/http";
-import {Observable, of, tap} from "rxjs";
+import {catchError, Observable, of, shareReplay, tap, throwError} from "rxjs";
 import {SearchResultInvestmentModel} from "../models/searchResultInvestment.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvestmentsService {
-  apiUrl = environment.investmentsEndpoint;
-  private investmentsCache: SearchResultInvestmentModel | null = null;
+
+  private apiUrl = environment.investmentsEndpoint;
+  private cache$: Observable<SearchResultInvestmentModel> | null = null;
+  private cacheExpirationMs = 100000; // Cache expiration in milliseconds (e.g., 300000ms = 5 minutes)
+  private lastRequestTime: number | null = null;
 
   constructor(private http: HttpClient) { }
 
   getInvestments(): Observable<SearchResultInvestmentModel> {
-    if (this.investmentsCache) {
+    const currentTime = new Date().getTime();
+
+    if (this.cache$ && this.lastRequestTime && (currentTime - this.lastRequestTime < this.cacheExpirationMs)) {
       console.log('Fetching data from cache Investments');
-      return of(this.investmentsCache); // Return cached data
+      return this.cache$;
     } else {
       console.log('Fetching data from API Investments');
-      return this.http.get<SearchResultInvestmentModel>(`${this.apiUrl}`)
-        .pipe(tap(data => this.investmentsCache = data)); // Cache data
+      this.cache$ = this.http.get<SearchResultInvestmentModel>(this.apiUrl).pipe(
+        tap(() => this.lastRequestTime = new Date().getTime()),
+        shareReplay(1),
+        catchError(error => {
+          console.error('Error fetching investments data:', error);
+          return throwError(() => error);
+        })
+      );
+      return this.cache$;
     }
   }
-  //TODO cache mechanizm angular
-  //TODO once hour check - shot to DB
 
 }
 
