cfab6294f07a4b64e2846d22fbe6a36339b2a4e2
michalswig <mike@data.pl>
2024-02-07 11:01:08 +0100

    Refactor Home and Contact Component

--- src-angular/src/app/modules/contact/contact.component.ts
+++ src-angular/src/app/modules/contact/contact.component.ts
@@ -1,17 +1,21 @@
 import {Component, OnInit} from '@angular/core';
-import {CodeStatusService} from "../core/services/code-status.service";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit{
+export class ContactComponent implements OnInit {
 
-constructor(private codeStatus: CodeStatusService) { }
+  private contactComponentTrigger: Subject<void> = new Subject<void>();
 
   ngOnInit(): void {
-    this.codeStatus.triggerContactComponentLoading();
+    this.triggerContactComponentLoading();
+  }
+
+  triggerContactComponentLoading() {
+    this.contactComponentTrigger.next();
   }
 
 }
--- src-angular/src/app/modules/core/services/code-status.service.ts
+++ src-angular/src/app/modules/core/services/code-status.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../../../environments/environment.development";
 import {HttpClient} from "@angular/common/http";
-import {Observable, Subject} from "rxjs";
+import {Observable} from "rxjs";
 import {SearchResultCode} from "../models/searchResultCode.model";
 
 
@@ -11,8 +11,6 @@ import {SearchResultCode} from "../models/searchResultCode.model";
 export class CodeStatusService {
 
   private apiSystemCode = environment.systemCodeEndpoint;
-  private contactComponentTrigger = new Subject<void>();
-  private homeComponentTrigger: Subject<void> = new Subject<void>();
 
   constructor(private http: HttpClient) {
   }
@@ -21,15 +19,4 @@ export class CodeStatusService {
     return this.http.get<SearchResultCode>(`${this.apiSystemCode}`);
   }
 
-  triggerContactComponentLoading() {
-    this.contactComponentTrigger.next();
-  }
-
-  getContactComponentTrigger() {
-    return this.contactComponentTrigger.asObservable();
-  }
-
-  getHomeComponentTrigger() {
-    return this.homeComponentTrigger.asObservable();
-  }
 }
