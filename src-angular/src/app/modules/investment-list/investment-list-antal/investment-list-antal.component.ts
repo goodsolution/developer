a1c801c6028ccb2c04c95d5e31794f9ceb37e5ce
michalswig <mike@data.pl>
2024-02-14 11:25:45 +0100

    adjust ANTAL for added component reloading

--- src-angular/src/app/modules/investment-list/investment-list-antal/investment-list-antal.component.ts
+++ src-angular/src/app/modules/investment-list/investment-list-antal/investment-list-antal.component.ts
@@ -1,80 +1,105 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {InvestmentResponse} from "../../core/models/investment.model";
 import {CityResponse} from "../../core/models/city.model";
-import {Subject, takeUntil} from "rxjs";
+import {Subject, Subscription, takeUntil} from "rxjs";
 import {InvestmentsService} from "../../core/services/investments.service";
 import {ActivatedRoute} from "@angular/router";
 import {CitiesService} from "../../core/services/cities.service";
+import {DynamicComponentLoadingService} from "../../core/services/dynamic-component-loading.service";
 
 @Component({
   selector: 'app-investment-list-antal',
   templateUrl: './investment-list-antal.component.html',
   styleUrls: ['./investment-list-antal.component.scss']
 })
-export class InvestmentListAntalComponent implements OnInit {
-  allInvestments: InvestmentResponse[] = []; // Holds the complete list of investments
-  investments: InvestmentResponse[] = []; // Used for displaying filtered/unfiltered data
+export class InvestmentListAntalComponent implements OnInit, OnDestroy {
+  allInvestments: InvestmentResponse[] = [];
+  investments: InvestmentResponse[] = [];
   cities: CityResponse[] = [];
   cityName: string = '';
   private unsubscribe$ = new Subject<void>();
+  private subscriptionToCityChanges?: Subscription;
+  private citiesLoaded = false;
 
-  constructor(private service: InvestmentsService, private route: ActivatedRoute, private cityService: CitiesService) {
+  constructor(
+    private dynamicLoadingService: DynamicComponentLoadingService,
+    private investmentsService: InvestmentsService,
+    private cityService: CitiesService,
+  ) {
   }
 
-  getInvestments() {
-    this.service.getInvestments()
+  ngOnInit(): void {
+    this.loadCities();
+  }
+
+  private loadCities() {
+    console.log('InvestmentListDodeComponent loadCities start');
+    if (this.citiesLoaded) {
+      return;
+    }
+    this.cityService.getCities()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: data => {
+          console.log('Cities loaded:', data.cities);
+          this.cities = data.cities;
+          this.citiesLoaded = true;
+          if (!this.subscriptionToCityChanges) {
+            this.subscribeToCityChanges();
+          }
+        },
+        error: error => {
+          console.error('Error fetching cities:', error);
+          this.citiesLoaded = false;
+        }
+      });
+    console.log('InvestmentListDodeComponent loadCities stop');
+  }
+
+  private subscribeToCityChanges() {
+    this.dynamicLoadingService.getInvestmentComponentTrigger()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(data => {
+        if (this.citiesLoaded && data.cityName !== this.cityName) {
+          this.cityName = data.cityName;
+          this.getInvestments();
+        }
+      });
+  }
+
+  private getInvestments() {
+    this.investmentsService.getInvestments()
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe({
         next: data => {
           this.allInvestments = data.investments;
-          this.investments = data.investments;
-          this.filterInvestmentsByCity(this.cityName); // Apply initial filter
+          this.filterInvestmentsByCity(this.cityName);
         },
         error: error => console.error('Error fetching investments:', error)
       });
   }
 
-  getCities() {
-    this.cityService.getCities()
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe({
-        next: data => this.cities = data.cities,
-        error: error => console.error('Error fetching cities:', error)
-      });
-  }
-
-  ngOnInit(): void {
-    this.getCities();
-    this.getInvestments(); // This should populate allInvestments
-    this.route.paramMap.subscribe(params => {
-      this.cityName = params.get('name') ?? '';
-      // Ensure that investments are loaded before filtering
-      if (this.allInvestments.length > 0) {
-        this.filterInvestmentsByCity(this.cityName);
+  private filterInvestmentsByCity(cityName: string): void {
+    if (!cityName) {
+      this.investments = [...this.allInvestments];
+      return;
+    }
+    this.investments = this.allInvestments.filter(investment => {
+      const city = this.cities
+        .find(c => c.name.toLowerCase() === cityName.toLowerCase());
+      if (!city) {
+        console.warn(`City not found: ${cityName}`);
       }
+      return city && investment.cityId === city.id;
     });
   }
 
-  filterInvestmentsByCity(cityName: string): void {
-    if (!cityName) {
-      // Handle the case where no city is selected initially
-      this.investments = [...this.allInvestments];
-      console.log('No city selected, copy all investments')
-      return;
-    }
-    const city = this.cities.find(c => c.name === cityName);
-    if (city) {
-      this.investments = this.allInvestments.filter(investment => investment.cityId === city.id);
-      console.log(`Found ${this.investments.length} investments for city ${cityName}`)
-    } else {
-// If no matching city is found, you can choose to either display all investments or none
-      this.investments = [...this.allInvestments]; // or handle differently, e.g., this.investments = [];
-      console.log('No matching city found, displaying all investments')
-    }
-  }
-
   ngOnDestroy(): void {
+    if (this.subscriptionToCityChanges) {
+      this.subscriptionToCityChanges.unsubscribe();
+    }
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
   }
+
 }
