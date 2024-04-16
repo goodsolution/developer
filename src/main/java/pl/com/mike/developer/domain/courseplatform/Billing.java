89aeb5fe20531b1e4020e351b0521a2195857d3f
michalswig <mike@data.pl>
2024-04-16 12:02:24 +0200

    clean project

--- src/main/java/pl/com/mike/developer/domain/courseplatform/Billing.java
+++ src/main/java/pl/com/mike/developer/domain/courseplatform/Billing.java
@@ -1,6 +1,6 @@
 package pl.com.mike.developer.domain.courseplatform;
 
-import pl.com.mike.developer.api.courseplatform.OrderCoursesInBasketPostRequest;
+
 
 public class Billing {
     private String type;
@@ -11,17 +11,17 @@ public class Billing {
     private String city;
     private String nip;
     private String country;
-
-    public Billing(OrderCoursesInBasketPostRequest request) {
-        this.type = request.getInvoiceType();
-        this.firstAndLastName = request.getInvoiceFirstAndLastName();
-        this.companyName = request.getInvoiceCompanyName();
-        this.street = request.getInvoiceStreet();
-        this.postalCode = request.getInvoicePostalCode();
-        this.city = request.getInvoiceCity();
-        this.nip = request.getInvoiceNip();
-        this.country = request.getInvoiceCountry();
-    }
+//
+//    public Billing(OrderCoursesInBasketPostRequest request) {
+//        this.type = request.getInvoiceType();
+//        this.firstAndLastName = request.getInvoiceFirstAndLastName();
+//        this.companyName = request.getInvoiceCompanyName();
+//        this.street = request.getInvoiceStreet();
+//        this.postalCode = request.getInvoicePostalCode();
+//        this.city = request.getInvoiceCity();
+//        this.nip = request.getInvoiceNip();
+//        this.country = request.getInvoiceCountry();
+//    }
 
     public Billing() {
     }
