4da378ea9dd8e5cdc19e99cf02973f0bd94afd1c
michalswig <mike@data.pl>
2024-05-23 11:26:38 +0200

    delete unused files

--- src/main/java/pl/com/mike/developer/web/developer/ConfigurationEndpoint.java
+++ src/main/java/pl/com/mike/developer/web/developer/ConfigurationEndpoint.java
@@ -1,4 +1,4 @@
-package pl.com.mike.developer.web;
+package pl.com.mike.developer.web.developer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
