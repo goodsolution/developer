d5020531e92f3529430308afb6682bba3ccd6749
michalswig <mike@data.pl>
2024-07-19 15:37:56 +0200

    refactor: project module

--- src-angular/src/app/modules/project-overview/project-overview.module.ts
+++ src-angular/src/app/modules/project-overview/project-overview.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProjectOverviewRoutingModule } from './project-overview-routing.module';
 import { ProjectOverviewComponent } from './project-overview.component';
 import {SharedModule} from "../shared/shared.module";
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ScreenshotDialogComponent } from './screenshot-dialog/screenshot-dialog.component';
+import {FooterComponent} from "./footer-project-overview/footer.component";
+import {HeaderComponent} from "./header-project-overview/header.component";
 
 
 @NgModule({
@@ -22,6 +22,9 @@ import { ScreenshotDialogComponent } from './screenshot-dialog/screenshot-dialog
     CommonModule,
     SharedModule,
     ProjectOverviewRoutingModule
+  ],
+  exports: [
+    ProjectOverviewComponent
   ]
 })
 export class ProjectOverviewModule { }
