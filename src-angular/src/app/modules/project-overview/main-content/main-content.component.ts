26fdd3da54aff8aa2e594ddfcaea22d283fd4bb1
michalswig <mike@data.pl>
2024-07-22 12:25:29 +0200

    refactor: project module styles

--- src-angular/src/app/modules/project-overview/main-content/main-content.component.ts
+++ src-angular/src/app/modules/project-overview/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ScreenshotDialogComponent} from "../screenshot-dialog/screenshot-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
 
