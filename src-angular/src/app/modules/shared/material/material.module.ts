7bb88cd7c8d4e00e5e4dc8ea1390b83075e023bd
michalswig <mike@data.pl>
2024-07-31 12:17:04 +0200

    add: tooltip to header

--- src-angular/src/app/modules/shared/material/material.module.ts
+++ src-angular/src/app/modules/shared/material/material.module.ts
@@ -13,6 +13,7 @@ import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatExpansionModule} from "@angular/material/expansion";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
   declarations: [],
@@ -30,7 +31,8 @@ import {MatExpansionModule} from "@angular/material/expansion";
     MatSidenavModule,
     MatListModule,
     MatDialogModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
