dec6a13ceca913ddd0ce320f6105a8125d884dfd
michalswig <mike@data.pl>
2024-08-07 12:16:13 +0200

    add: Premise Generator

--- src/main/java/pl/com/mike/developer/Application.java
+++ src/main/java/pl/com/mike/developer/Application.java
@@ -6,19 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import pl.com.mike.developer.logic.developer.PremiseService;
 
 @SpringBootApplication
 @EntityScan(
         basePackageClasses = {Application.class, Jsr310JpaConverters.class}
 )
 public class Application extends SpringBootServletInitializer {
-
-    private final PremiseService premiseService;
-
-    public Application(PremiseService premiseService) {
-        this.premiseService = premiseService;
-    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
