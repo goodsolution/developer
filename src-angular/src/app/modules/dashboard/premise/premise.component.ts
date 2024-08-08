930e9f5ef20ea9f4ec27b28d0c239d6bead87232
michalswig <mike@data.pl>
2024-08-08 11:50:10 +0200

    add: pagination premise list in dashboard section

--- src-angular/src/app/modules/dashboard/premise/premise.component.ts
+++ src-angular/src/app/modules/dashboard/premise/premise.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { DeveloperResponse } from "../../core/models/developer.model";
 import { PremiseResponse } from "../../core/models/premise.model";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { PremiseService } from "../../core/services/premise.service";
 import { DeveloperService } from "../../core/services/developer.service";
+import { MatPaginator } from "@angular/material/paginator";
 
 @Component({
   selector: 'app-premise',
@@ -15,9 +16,15 @@ export class PremiseComponent implements OnInit {
   developers: DeveloperResponse[] = [];
   selectedDeveloper: DeveloperResponse | null = null;
   premises: PremiseResponse[] = [];
+  pagedPremises: PremiseResponse[] = [];
   selectedPremise: PremiseResponse | null = null;
   premiseForm: FormGroup;
   isNewPremise: boolean = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  currentPage: number = 0;
+  pageSize: number = 5;
 
   constructor(
     private fb: FormBuilder,
@@ -54,8 +61,20 @@ export class PremiseComponent implements OnInit {
     if (this.selectedDeveloper) {
       this.premiseService.getPremisesByInvestmentId(this.selectedDeveloper.id).subscribe((response) => {
         this.premises = response.premisesGetResponse;
+        this.updatePagedPremises();
       });
     }
+  }
+
+  onPageChange(event: any): void {
+    this.currentPage = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.updatePagedPremises();
+  }
+
+  updatePagedPremises(): void {
+    const startIndex = this.currentPage * this.pageSize;
+    this.pagedPremises = this.premises.slice(startIndex, startIndex + this.pageSize);
   }
 
   addNewPremise(): void {
